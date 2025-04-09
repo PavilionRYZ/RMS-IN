ef0488a029f60e8d609c4ee5ed27e3b164a9c21d
Subhra sundar sinha <72126523+PavilionRYZ@users.noreply.github.com>
2025-04-09 12:58:05 +0530

    `Update Loading component layout and image container`

--- Frontend/src/components/Loading/Loading.jsx
+++ Frontend/src/components/Loading/Loading.jsx
@@ -1,8 +1,8 @@
 import loading from '../../assets/infinite-spinner.svg'
 const Loading = () => {
   return (
-    <div>
-        <img src={loading} alt="Loading..." />
+    <div className='flex justify-center items-center h-screen'>
+        <img className='w-25 h-25' src={loading} alt="Loading..." />
     </div>
   )
 }
